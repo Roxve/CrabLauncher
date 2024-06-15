733628169e6e5b0424448d7b3d9b56100673736f
Roxve <116169309+Roxve@users.noreply.github.com>
2024-06-15 13:14:06 +0300

    a bit better code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::{
     collections::HashMap,
     fs::{self, File},
-    io::Write,
+    io::{Read, Write},
 };
 
 use reqwest;
-use serde_json::Value;
+use serde_json::{Map, Value};
 
 #[derive(Debug)]
 pub enum VersionKind {
@@ -22,66 +22,101 @@ pub struct Version {
 #[derive(Debug)]
 pub struct Env {
     pub versions: HashMap<String, Version>,
-    pub latest: String,
+    pub latest_release: String,
+    pub latest_snapshot: String,
 }
 
 impl Env {
-    pub fn new(versions: HashMap<String, Version>, latest: String) -> Self {
-        Self { versions, latest }
+    pub fn new(
+        versions: HashMap<String, Version>,
+        latest_release: String,
+        latest_snapshot: String,
+    ) -> Self {
+        Self {
+            versions,
+            latest_release,
+            latest_snapshot,
+        }
+    }
+
+    pub fn from_manifest(manifest: Map<String, Value>) -> Self {
+        let mut versions: HashMap<String, Version> = HashMap::new();
+
+        let manifest_versions = manifest["versions"].as_array().unwrap();
+
+        for obj in manifest_versions {
+            let obj = obj.as_object().unwrap();
+            let id = obj["id"].as_str().unwrap().to_string();
+
+            let kind = match &obj["type"] {
+                Value::String(s) => match s.as_str() {
+                    "release" => VersionKind::Release,
+                    "snapshot" => VersionKind::Snapshot,
+                    "old_alpha" => VersionKind::OldAlpha,
+                    "old_beta" => VersionKind::OldBeta,
+                    k => panic!("unknown version type {}", k),
+                },
+
+                k => panic!("invaild version Value type {}", k),
+            };
+
+            let url = obj["url"].as_str().unwrap().to_string();
+            let version = Version { kind, url };
+
+            versions.insert(id, version);
+        }
+        let manifest_latest = manifest["latest"].as_object().unwrap();
+
+        let manifest_latest_release = manifest_latest["release"].as_str().unwrap().to_string();
+        let manifest_latest_snapshot = manifest_latest["snapshot"].as_str().unwrap().to_string();
+
+        Env::new(versions, manifest_latest_release, manifest_latest_snapshot)
     }
 }
 
-fn main() {
+fn init() {
     fs::create_dir_all("launcher/ibs").expect("failed to create libs folder");
     fs::create_dir_all("launcher/assests").expect("failed to create assest folder");
+}
 
+fn init_manifest() -> Map<String, Value> {
     // download version info
     let res =
-        reqwest::blocking::get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
-            .expect("failed downloading minecraft version manifest");
+        reqwest::blocking::get("https://launchermeta.mojang.com/mc/game/version_manifest.json");
+    // if offline use pre-downloaded file
+    let manifest = if res.is_ok() {
+        let res = res.unwrap();
 
-    let buffer = res.text().unwrap();
-    let bytes = buffer.as_bytes();
+        let buffer = res.text().unwrap();
+        let bytes = buffer.as_bytes();
 
-    let mut manifest = File::create("launcher/version_manifest.json").unwrap();
+        let mut manifest_file = File::create("launcher/version_manifest.json").unwrap();
+
+        manifest_file
+            .write_all(bytes)
+            .expect("failed writing file version_manifest.json");
+
+        serde_json::from_str(buffer.as_str()).expect("failed reading file version_manifest.json")
+    } else {
+        let mut manifest_file = File::open("launcher/version_manifest.json").expect(
+            "failed to load version_manifest.json, please connect to the internet and try again",
+        );
+
+        let mut buffer = String::new();
+        manifest_file
+            .read_to_string(&mut buffer)
+            .expect("opened version_manifest.json, but failed reading it");
+
+        serde_json::from_str(buffer.as_str()).expect("failed parsing file version_manifest.json")
+    };
 
     manifest
-        .write_all(bytes.clone())
-        .expect("failed writing file version_manifest.json");
+}
 
-    let manifest: serde_json::Value =
-        serde_json::from_str(buffer.as_str()).expect("failed reading file version_manifest.json");
+fn main() {
+    init();
+    let manifest = init_manifest();
 
-    let manifest = manifest.as_object().unwrap();
-    let mut versions: HashMap<String, Version> = HashMap::new();
-
-    for obj in manifest["versions"].as_array().unwrap() {
-        let obj = obj.as_object().unwrap();
-        let id = obj["id"].as_str().unwrap().to_string();
-
-        let kind = match &obj["type"] {
-            Value::String(s) => match s.as_str() {
-                "release" => VersionKind::Release,
-                "snapshot" => VersionKind::Snapshot,
-                "old_alpha" => VersionKind::OldAlpha,
-                "old_beta" => VersionKind::OldBeta,
-                k => panic!("unknown version type {}", k),
-            },
-
-            k => panic!("invaild version Value type {}", k),
-        };
-
-        let url = obj["url"].as_str().unwrap().to_string();
-        let version = Version { kind, url };
-
-        versions.insert(id, version);
-    }
-
-    let env = Env::new(
-        versions,
-        manifest["latest"].as_object().unwrap()["release"]
-            .as_str()
-            .unwrap()
-            .to_string(),
-    );
+    let env = Env::from_manifest(manifest);
+    dbg!(&env);
 }
